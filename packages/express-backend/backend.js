05164d8e3bd6e33c16143b22daebf6c75319c328
Gabe-Alonso <gabixtreme363@outlook.com>
2024-01-31 19:02:58 -0800

    Ex 3 Step 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -50,8 +51,9 @@ const deleteUser = (id) => {
     users["users_list"] = users["users_list"].filter(user => user.id !== id);
 };
 
-
+app.use(cors());
 app.use(express.json());
+
   
 
 
